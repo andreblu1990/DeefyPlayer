ead9a2747a2bbd43cdbc81e51f480a3981a45b73
Demitrius <demitrius.quadros@outlook.com>
2022-03-19 14:01:57 -0300

    Cricao do crud no banco de dados

---
+++ DeeFyPlay/Controllers/UsuarioController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DeeFyPlay.Infra;
+using DeeFyPlay.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeeFyPlay.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsuarioController : Controller
+    {
+        private readonly DeeFyDbContext _context;
+
+        public UsuarioController(DeeFyDbContext context)
+        {
+            _context = context;    
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Usuario> Get()
+        {
+            return _context.Usuarios.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Usuario Get(int id)
+        {
+            return _context.Usuarios.Find(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Usuario usuario)
+        {
+            usuario.Id = 0;
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+        }
+
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Usuario usuario)
+        {
+            usuario.Id = id;
+            _context.Update(usuario);
+            _context.SaveChanges();
+        }
+
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var usuario = _context.Usuarios.Find(id);
+
+            if (usuario != null)
+            {
+                _context.Remove(usuario);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
